a5c6ce93ae958ed682d53036cedfe6d3b51ee059
A Sorokin <lptolik@gmail.com>
2017-07-09 17:17:28 +0300

    feature qualtiy table for quality tab

--- R/featureTest/server.R
+++ R/featureTest/server.R
@@ -17,7 +17,66 @@ loadFeature <- function(fname, env = new.env()) {
   return(env)#data.table(env$pdts))
 }
 
+#' Calculate quality tab for particular feature
+#'
+#' @param p
+#' @param ion
+#' @param ppm
+#' @param minClusterSize
+#'
+#' @return
+#' @export
+#'
+#' @examples
+
+featureQTab<-function(p,
+                      ions,
+                      ppm = 20,
+                      minClusterSize=30,
+                      minPTS=20){
+  qualityTab<-data.frame(ion=maxMZ,
+                         medOutlier=NA,
+                         numClass=NA,
+                         numZero=NA,
+                         N=NA,
+                         ppmSD=NA,
+                         ppmRange=NA,
+                         ppmIQR=NA)
+  for(i in 1:length(ions)){
+    ion<-ions[i]
+    idxIon<-which((p$ion-ion)<1e-5)
+    cP<-which.max(p$intensity[idxIon])
+    ionI<-idxIon[cP]
+    maxMZ<-p$mz[ionI]
+    idx <- which(abs(p$mz - maxMZ) < (ppm * 1e-6 * maxMZ))
+    p. <- p[idx, ]
+    rl <- get_runLen(p$scan[idxIon], scanLen)
+    l<-lm(mz ~ time, data = p[idxIon, ])
+    range(l$residuals[abs(rstandard(l)) <= 2]) -> bnd
+    pl <- predict(l, p.)
+    rl <- p.$mz - pl
+    idL <- which(rl >= bnd[1] & rl <= bnd[2])
+    ct2 <- hdbscan(scale(p.[,c('mz')]), minPts = min(minPTS,dim(p.)[1]/4))
+    qualityTab$medOutlier[i]<-median(ct2$outlier_scores,na.rm = TRUE)
+    qualityTab$numClass[i]<-max(ct2$cluster)
+    qualityTab$numZero[i]<-length(which(ct2$cluster==0))
+    qualityTab$N[i]<-dim(p.)[1]
+    qualityTab$ppmSD[i]<-1e6*sd(p.$mz,na.rm = TRUE)/maxMZ
+    qualityTab$ppmRange[i]<-1e6*diff(range(p.$mz))/maxMZ
+    qualityTab$ppmIQR[i]<-1e6*IQR(p.$mz,na.rm = TRUE)/maxMZ
+  }
+  qualityTab$logOutlier<-log10(qualityTab$medOutlier)
+  qualityTab$Range2SD<-qualityTab$ppmRange/qualityTab$ppmSD
+  qualityTab$SD2IQR<-qualityTab$ppmSD/qualityTab$ppmIQR
+  return(qualityTab)
+}
+
+plotFeature<-function(){
+  
+}
+
 load("def.feature.Rdat")
+qN<-10
 options(shiny.maxRequestSize=50*1024^2) 
 
 # Define server logic required to draw a histogram
