58df119731317a3ce9ee4eb36801c2cc2b88d912
A Sorokin <lptolik@gmail.com>
2017-11-08 10:36:25 +0300

    scan selection app

---
+++ R/analyzeSpectra/server.R
@@ -0,0 +1,164 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(RColorBrewer)
+library(ggplot2)
+library(MALDIquant)
+library(MALDIquantForeign)
+
+options(shiny.maxRequestSize=500*1024^2) 
+
+plotSNR<-function(sp){
+  snrs <- c(seq(from=1, to=2.5, by=0.5),5,10)
+  ## define different colors for each step 
+  col <- rainbow(length(snrs))
+  ## estimate noise
+  md<-metaData(sp)
+  noise <- MALDIquant::estimateNoise(sp,
+                                     method="SuperSmoother")
+  maxNoise<-max(noise[,2])*max(snrs)
+  maxSig<-max(sp@intensity)
+  plot(sp,
+       ylim=c(0,max(maxNoise,maxSig)),
+       main=paste0('I=',md$number,', t=',round(md$retentionTime,3)))
+  for (i in seq(along=snrs)) { lines(noise[, "mass"],
+                                     noise[, "intensity"]*snrs[i],
+                                     col=col[i], lwd=2)
+  }
+  legend("topright", legend=snrs, col=col, lwd=1)
+}
+
+plotBaseLine<-function(sp){
+  iterations <- seq(from=25, to=100, by=25) ## define different colors for each step 
+  col <- rainbow(length(iterations))
+  md<-metaData(sp)
+  plot(sp,
+       main=paste0('I=',md$number,', t=',round(md$retentionTime,3)))
+  ## draw different baseline estimates
+  for (i in seq(along=iterations)) {
+    baseline <- estimateBaseline(sp, method="SNIP",
+                                 iterations=iterations[i])
+    lines(baseline, col=col[i], lwd=2)
+  }
+  legend("topright", legend=iterations, col=col, lwd=1)
+}
+
+defSpectra<-list(
+  createMassSpectrum(
+    mass=sort(sample(x = 100:1300,size = 1000,replace = FALSE)),
+    intensity = rep(0,1000),
+    metaData = list(name='default empty spectra')))
+
+halfWindowSize<-5
+iterations <- 100 
+## define different colors for each step 
+col <- rainbow(length(iterations))
+
+
+# Define server logic required to draw a histogram
+shinyServer(function(input, output) {
+# Global identifier of the scan to play with
+     scanID<-reactive({return(input$sID)})
+# Global RV for data
+     dataRV<-reactiveValues(ncMD=list(),spectra=defSpectra,TIC=0,XIC100=1e-7,XIC400=1e-7,time=1,scans=c(1:4))
+
+# Load spectra from file
+  observeEvent(input$inFile,{
+    inFile <- input$inFile
+    if (is.null(inFile))
+      return(defSpectra)
+    cat(names(inFile),'\n')
+    cat('name: ',inFile$name,', size: ',inFile$size,', type: ',inFile$type,', datapath: ',inFile$datapath,'\n')
+    #dataRV$fncMD<- ncatt_get(nc_open(inFile$datapath),0)
+    spectra<-import(inFile$datapath,verbose=FALSE)
+    tic<-sapply(spectra,totalIonCurrent)
+    spectra400 <- trim(spectra,range = c(400,1000))
+    spectra100 <- trim(spectra,range = c(100,400))
+    XIC100<-sapply(spectra100,totalIonCurrent)
+    XIC400<-sapply(spectra400,totalIonCurrent)
+    time<-sapply(spectra,function(.x)metaData(.x)$retentionTime)
+    dataRV$spectra<-spectra
+    dataRV$ind<-sapply(spectra,function(.x)metaData(.x)$number)
+    dataRV$time<-time
+    dataRV$tic<-tic
+    dataRV$XIC100<-XIC100
+    dataRV$XIC400<-XIC400
+  })
+  
+  observeEvent(input$tic_dblclick, {
+    brush <- input$tic_brush
+    if (!is.null(brush)) {
+      dataRV$scans <- c(brush$xmin, brush$xmax)
+      cat('dataRV$scans',dataRV$scans,'\n')
+    } else {
+      dataRV$scans <- NULL
+    }
+  })
+  
+  observeEvent(input$xic_dblclick, {
+    brush <- input$xic_brush
+    if (!is.null(brush)) {
+      dataRV$scans <- c(brush$xmin, brush$xmax)
+      cat('dataRV$scans',dataRV$scans,'\n')
+    } else {
+      dataRV$scans <- NULL
+    }
+  })
+  
+  
+  output$ticPlot<-renderPlot({
+    qtic<-quantile(dataRV$tic,probs = seq(0, 1, 0.1),names = TRUE)
+    ctic<-factor(findInterval(dataRV$tic,qtic),labels = names(qtic))
+    qplot(dataRV$time,dataRV$tic,colour=ctic,shape = ".")+
+      scale_colour_brewer(palette = 'Spectral')+
+      geom_line(aes(group=1))
+  })
+  output$xicPlot<-renderPlot({
+    df<-rbind(data.frame(time=dataRV$time,XIC=dataRV$XIC100,type='XIC100'),
+              data.frame(time=dataRV$time,XIC=dataRV$XIC400,type='XIC400'))
+    qplot(time,XIC,colour=type,data=df,shape = ".")+
+      scale_colour_brewer(palette = 'Spectral')+
+      geom_line()
+  })
+  output$blPlot<-renderPlot({
+    if(input$corBL){
+      plot(spSelected())
+    }else{
+      plotBaseLine(spSelected())
+    }
+  })
+  
+  spSelected<-reactive({
+    spId<-input$sID
+    sc<-input$scale
+    nr<-input$norm
+    bl<-input$corBL
+    if(spId>length(dataRV$spectra)){
+      spId<-length(dataRV$spectra)
+    }
+    sp<-dataRV$spectra[[spId]]
+    if(sc){    
+      sp<-transformIntensity(sp, method="sqrt")
+    }
+    sp <- MALDIquant::smoothIntensity(sp, method="SavitzkyGolay",halfWindowSize=halfWindowSize)
+    if(nr){
+    sp <- MALDIquant::calibrateIntensity(sp, method="TIC")
+    }
+    if(bl){
+      sp<- MALDIquant::removeBaseline(sp, method="SNIP",iterations=iterations)
+    }
+    return(sp)
+  })
+  
+  output$snrPlot<-renderPlot({
+    cat('snrPlot:',length(spSelected()),metaData(spSelected())$number,input$sID,'\n')
+    plotSNR(spSelected())   
+  })
+})
---
+++ R/analyzeSpectra/ui.R
@@ -0,0 +1,50 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+library(shiny)
+library(shinydashboard)
+
+# Define UI for application that draws a histogram
+dashboardPage(
+  dashboardHeader(title = 'Data analysis'),
+  dashboardSidebar(width = 350,
+                   sidebarMenu(
+                    # menuItem("Load data",tabName = 'File'),
+                     menuItem("TIC plot",tabName = 'TIC'),
+                     menuItem("XIC plots",tabName = 'XIC'),
+                     menuItem("Baseline",tabName = 'baseline'),
+                     menuItem("SNR",tabName = 'snr'),
+                     fileInput('inFile', 'Choose CDF File'),
+                     sliderInput("sID",'Scan',min=1,max=2000,value = 1),
+                     checkboxInput("scale", label = "Scale spectra", value = TRUE),
+                     checkboxInput("corBL", label = "Correct baseline", value = TRUE),
+                     checkboxInput("norm", label = "Normalize spectra", value = TRUE)
+                   )),
+  dashboardBody(
+    tabItems(
+      # tabItem('File',
+      #         fluidRow(column(width = 12,
+      #                         fileInput('inFile', 'Choose CDF File'))),
+      #         fluidRow(column(width = 12,
+      #                         DT::dataTableOutput("featuresRev",width = '80%')))
+      # ),
+      tabItem('TIC',plotOutput("ticPlot", height = 800,dblclick = "tic_dblclick",
+                               brush = brushOpts(id = "tic_brush",
+                                                 resetOnNew = TRUE))),
+      tabItem('XIC',plotOutput("xicPlot", height = 800,dblclick = "xic_dblclick",
+                               brush = brushOpts(id = "xic_brush",
+                                                 resetOnNew = TRUE))),
+      tabItem("baseline",plotOutput("blPlot", height = 800)),
+      tabItem("snr",plotOutput("snrPlot", height = 800))
+    )
+  ),
+  title = "Dashboard example"
+)
