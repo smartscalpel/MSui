d30190aa015f9c1946ef1b0d58acc28c196922f1
lptolik <lptolik@gmail.com>
2018-03-16 14:51:20 +0300

    escape special characters in file names

--- load2db.R
+++ load2db.R
@@ -17,26 +17,27 @@ path<-'/var/samba/share/Scalpel/BD_cdf/'
 #########################################
 
 fl<-dir(path=path,pattern='*.cdf$',recursive=TRUE)
-
+wd<-getwd()
 for(f in fl){
 #  ls()->L
 #  rm(list=L[!L%in%c('fl','f','rmd.name')])
   dir.name<-sub('.cdf','',basename(f))
-  rmd.name<-sub('cdf$','Rmd',basename(f))
-  system(paste0('mkdir -p ./',dir.name))
-  cat(f,'\n',dir.name,'\n')
-  rmd.fname<-paste0(getwd(),'/',dir.name,'/',rmd.name)
+  dir.name<-gsub("(\"|'|\\(|\\)|\\[|\\]| |\t)",'_',dir.name)
+  rmd.name<-paste0(dir.name,'.Rmd')
+  system(paste0('mkdir -p ./',shQuote(dir.name)))
+  cat('cdf:',f,'\ndir:',dir.name,'\nrmd:',rmd.name,'\n')
+  rmd.fname<-paste0('./',dir.name,'/',rmd.name)
+  cat('fname',rmd.fname,'\n')
   if(!file.exists(rmd.fname)){
-    file.copy('load2scalpelDB.Rmd',rmd.fname)
+    file.copy('../load2scalpelDB.Rmd',rmd.fname)
     if(FALSE){#file.exists(rmd.name)){
       rmarkdown::render(rmd.fname,'pdf_document')
     }
   }
+  rmd.fname<-normalizePath(rmd.fname)
   if(!file.exists(sub('Rmd','pdf',rmd.fname))){
     cdf.file<-normalizePath(paste0(path,f))
-    system(paste0('cd ./',shQuote(dir.name)))
-    try(rmarkdown::render(rmd.name,'pdf_document'),FALSE,outFile=sub('Rmd$','try.out',rmd.name))
-    system('cd ../')
+    try(rmarkdown::render(rmd.fname,'pdf_document'),FALSE,outFile=sub('Rmd$','try.out',rmd.fname))
   }
 }
 
