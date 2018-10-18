b65f1a51e0c89c76e7c7c10b36d3aab03c56a7ba
lptolik <lptolik@gmail.com>
2018-10-18 21:03:30 +0300

    pathway template

---
+++ path_template.R
@@ -0,0 +1,10 @@
+############ Predefined parameters
+dbname = "msinvent"
+usr='msinvent'
+pwd='msinvent'
+tmpPath = '/var/workspaceR/dataloading/Burdenko'
+protocol<-data.frame(id=2,devID=4,solID=10,isID=5)
+path<- '/var/samba/share/Burdenko/'
+archPath = '/var/workspaceR/scalpelData/archive/Burdenko/'
+#########################################
+
