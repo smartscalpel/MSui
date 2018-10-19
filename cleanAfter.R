a742f6c9ca1c8b14262fc5cec5555d355aed56dd
lptolik <lptolik@gmail.com>
2018-10-19 20:16:03 +0300

    local cleaning fix

--- cleanAfter.R
+++ cleanAfter.R
@@ -6,9 +6,9 @@ if(!require(MonetDB.R)){stop('Library "MonetDB.R" is missing.\n')}
 dbname = "msinvent"
 usr='msinvent'
 pwd='msinvent'
-tmpPath = '/Volumes/AS_WD_HFS/Scalpel/Burdenko/' #'/var/workspaceR/scalpelData/data/'
+tmpPath = '/var/workspaceR/dataloading/Burdenko/' #'/var/workspaceR/scalpelData/data/'
 archPath = '/var/workspaceR/scalpelData/archive/Burdenko/'
-path<- '/Volumes/AS_Backup/Scalpel/Burdenko/'
+path<- '/var/samba/share/Burdenko/'
 getSpec<-paste('select s.id from spectrum s join patient p on s.sampletumorpatientid=p.id ',
                'join tissue t on s.sampletumorid=t.id',
                'join smpl l on s.sampleid=l.id',
@@ -20,8 +20,10 @@ wd<-getwd()
 conn <- dbConnect(MonetDB.R::MonetDB(), 
                   dbname = dbname,
                   user=usr,password=pwd)
+cat('connect, ',length(mfl),'metafiles\n')
 for(mf in mfl){
   wdir<-dirname(normalizePath(paste0(path,mf)))
+  cat('start ',wdir,'\n')
   mdt<-read.xlsx(paste0(path,mf))
   idx2Load<-which(mdt$type=='signal'&!is.na(mdt$status)&mdt$status=='good')
   mdt<-mdt[idx2Load,]
@@ -31,23 +33,28 @@ for(mf in mfl){
     rmdl<-dir(tdir,'prep.Rmd',recursive=TRUE)
     if(length(tsvl)==length(rmdl)){
       res<-TRUE
+      misL<-c()
       for(i in 1:dim(mdt)[1]){
         spid<-dbGetQuery(conn,getSpec,mdt$patient[i],mdt$tissue[i],mdt$sample[i])
         if(dim(spid)[1]==0){
           res<-FALSE
-          break
+          misL<-c(misL,paste0(tdir,'/',dirname(rmdl[i])))
         }
       }
       if(res){
+      cat('move ',wdir,' to archive\n')
       adir<-dirname(sub(path,archPath,wdir))
       system(paste0('mkdir -p ',adir))
       system(paste0('mv ',wdir,' ',adir))
+      }else{
+       cat('not loaded: \n',paste(misL,collapse='\n'),'\n')
       }
     }else{
       idx<-match(dirname(rmdl),dirname(tsvl))
-      cat('not loaded: \n',paste0(tdir,dirname(rmdl[is.na(idx)]),collapse = '\n'),'\n')
+      cat('not loaded: \n',paste0(tdir,'/',dirname(rmdl[is.na(idx)]),collapse = '\n'),'\n')
     }
   }else{
+    cat('move unprocessed ',wdir,' to archive\n')
     adir<-dirname(sub(path,archPath,wdir))
     system(paste0('mkdir -p ',adir))
     system(paste0('mv ',wdir,' ',adir))
