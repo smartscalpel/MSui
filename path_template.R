0cf1400f610e45c142e598afc07146964f2f4861
Anatoly Sorokin <lptolik@gmail.com>
2019-03-21 21:22:15 +0300

    preliminary working loader

--- path_template.R
+++ path_template.R
@@ -4,6 +4,8 @@ usr='msinvent'
 pwd='msinvent'
 protocolPath<-'/var/workspaceR/dataloading/'
 protocolName<-'190130'
+solID=10
+isID=5
 devID<-4 # LTQ_Burdenko
 #devID<-2 # Orbitrap_MIPT 
 tmpPath = '/var/workspaceR/dataloading/Burdenko'
