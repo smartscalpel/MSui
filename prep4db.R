8724b6da83864a064268d5b841fac5940dc8d038
Anatoly Sorokin <lptolik@gmail.com>
2019-03-22 15:02:07 +0300

    fix the case of empty header row in Excell

--- prep4db.R
+++ prep4db.R
@@ -29,6 +29,12 @@ wd<-getwd()
 for(mf in mfl){
   wdir<-dirname(normalizePath(paste0(path,mf)))
   mdt<-read.xlsx(paste0(path,mf))
+  if(any(grepl('(wash|bg)',names(mdt)))){# to fix crasy Excel without header row
+    cat(mf,'\n')
+    mdt<-read.xlsx(paste0(path,mf),colNames=FALSE)
+    while(dim(mdt)[2]<length(cnames)){mdt<-cbind(mdt,rep(NA,dim(mdt)[1]))}
+    names(mdt)<-cnames
+  }
   idx2Load<-which(mdt$type=='signal'&!is.na(mdt$status)&mdt$status=='good')
   mdt<-mdt[idx2Load,]
  # if(dim(mdt)[1]>0){
