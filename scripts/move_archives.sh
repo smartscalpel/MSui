0bc03488791811179ead7adf5755549f2c26f5d9
den <denis.zavorotnyuk@gmail.com>
2019-11-15 12:15:06 +0300

    added backuping

---
+++ scripts/move_archives.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "Move archives is started"
+
+TARGET_DIR=/var/workspaceR/scalpelData/archive/loaded_data
+FOLDER_TO_MONITOR=( Burdenko Neurosurgery )
+LIMP_STORAGE=/mnt/limpstorage/monetdb5
+
+wd=$(pwd)
+
+# shellcheck disable=SC2068
+for m_dir in ${FOLDER_TO_MONITOR[@]}; do
+  cur_target_dir=$TARGET_DIR/$m_dir
+  cur_source_dir=$wd/$m_dir
+  echo "Processing $cur_source_dir"
+  if [ ! -d "${cur_target_dir}" ]; then
+    if mkdir "$cur_target_dir"; then
+      echo "Target directory ${cur_target_dir} was created"
+    fi
+  fi
+  rsync -avhcEt --remove-source-files --include="*.tbz2" --exclude="*" "$cur_source_dir/" "$cur_target_dir"
+
+done
+if [ -d ${LIMP_STORAGE} ]; then
+  echo "Synchronization of DataBase backup"
+  rsync -avhcEt --remove-source-files --include="msinvent.*.tar" --include="msinvent.*.tar.gz" --exclude="*" "$wd/" "$LIMP_STORAGE"
+else
+  echo "${LIMP_STORAGE} is not accessible!"
+fi
